9fbec64e74fae831dbd4bfa3a6cb7991e2ca24ee
Kendra Regmi <41198661+kendraregmi@users.noreply.github.com>
2021-03-17 19:16:50 +0545

    Add files via upload

--- Assignment-2/problem7.py
+++ Assignment-2/problem7.py
@@ -3,3 +3,24 @@
 # average age, skipping over any None values. Print out each name,
 # followed by old or young if they are above or below the average age.
 
+tuple1 = ('Jon', 'Cena', 50)
+tuple2 = ('Randy', 'Orton', 35)
+tuple3 = ('George', 'Bush', None)
+tuple4 = ('HAppy', 'Singh', None)
+tuple5 = ('Hari', 'Mani', 80)
+tuples = [tuple1, tuple2, tuple3, tuple4, tuple5]
+ageSum = 0
+count = 0
+for t in tuples:
+    if t[2] is not None:
+        ageSum += t[2]
+        count += 1
+avg = ageSum / count
+for t in tuples:
+    if t[2] is not None:
+        if t[2] > avg:
+            print(t[0] + " " + t[1] + " " + "OLD")
+        else:
+            print(t[0] + " " + t[1] + " " + "YOUNG")
+    else:
+        print(t[0] + " " + t[1])
