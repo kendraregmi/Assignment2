2b1a9bdc1219dd65d57d379b5ac783ff533f8f30
Kendra Regmi <41198661+kendraregmi@users.noreply.github.com>
2021-03-17 19:13:28 +0545

    Add files via upload

--- Assignment-2/problem3.py
+++ Assignment-2/problem3.py
@@ -1,3 +1,18 @@
 # 3. Write code that will print out the anagrams (words that use the same
 # letters) from a paragraph of text.
 
+from collections import defaultdict 
+ 
+def printAnagramsTogether(words):
+    groupedWords = defaultdict(list)
+ 
+    for word in words:
+        groupedWords["".join(sorted(word))].append(word)
+ 
+    for group in groupedWords.values():
+        print(" ".join(group))      
+ 
+ 
+if __name__ == "__main__":   
+    arr =  ["cat", "dog", "tac", "god", "act"]  
+    printAnagramsTogether(arr)   
